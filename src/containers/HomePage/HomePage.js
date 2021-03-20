823f072747b32cabd524e6f92c99d40968b8f8ee
unknown <nguyenvanty10987654321000@gmail.com>
2021-03-20 22:32:06 +0700

    update 12346

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,16 +1,156 @@
-import React, { Component } from 'react';
-import Layout from '../../components/Layout/layout';
-
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { connect } from "react-redux";
+import "./styles.css";
+import Slider from "react-slick";
+import CarouselMulti from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ItemsCarousel from "react-items-carousel";
+import CreateIcon from "@material-ui/icons/Create";
+import * as actions from "./../../actions/index";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import { ImageTwoTone } from "@material-ui/icons";
+import Layout from "./../../components/Layout/layout";
+import FilterCenterFocusIcon from "@material-ui/icons/FilterCenterFocus";
+import SearchIcon from "@material-ui/icons/Search";
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 4,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                <Layout>
-                    Home Page
-                </Layout>
-            </div>
-        );
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    this.props.onTypeAPI();
+  }
+  render() {
+    var { type } = this.props;
+    console.log(type);
+    return (
+      <Layout>
+        {type.map((values, index) => {
+          return (
+            <div key={index}>
+              <Carousel
+                key={index}
+                className="home_carousel"
+                renderThumbs={() => {}}
+                autoPlay={true}
+                emulateTouch={true}
+                infiniteLoop={true}
+                interval={1750}
+              >
+                {values.banners &&
+                  values.banners.map((valuess, index) => {
+                    console.log(valuess.img);
+                    var image = require(`./../../assets/images/${valuess.img}`);
+                    return (
+                      <a
+                        href={valuess.navigateTo}
+                        style={{ display: "block" }}
+                        key={index}
+                      >
+                        <img src={image.default} alt="" />
+                      </a>
+                    );
+                  })}
+              </Carousel>
 
-export default HomePage;
+              {values.products.length <= 0 ? (
+                ""
+              ) : (
+                <div className="home_product">
+                  <div className="home_product_title">
+                    <div className="home_product_title_left">
+                      {values.title !== "" ? values.title : ""}
+                    </div>
+                    <button className="home_product_title_right">
+                      View All
+                    </button>
+                  </div>
+                  <div className="home_product_divide"></div>
+                  <CarouselMulti
+                    responsive={responsive}
+                    swipeable={false}
+                    draggable={false}
+                    // showDots={true}
+                    ssr={true} // means to render carousel on server-side.
+                    // infinite={true}
+                    transitionDuration={500}
+                    dotListClass="custom-dot-list-style"
+                    itemClass="carousel-item-padding-40-px"
+                    containerClass="carousel-container"
+                    keyBoardControl={true}
+                  >
+                    {values.products &&
+                      values.products.map((valuess, index1) => {
+                        var image = require(`./../../assets/images/${valuess.img}`);
+                        return (
+                          <div className="product" key={index1}>
+                            <div className="product__header">
+                              <img src={image.default} alt="" />
+                            </div>
+
+                            <ul>
+                              <li>
+                                <a>
+                                  <AddShoppingCartIcon className="product_icon" />
+                                </a>
+                              </li>
+                              <li>
+                                <a>
+                                  <SearchIcon className="product_icon" />
+                                </a>
+                              </li>
+                              <li>
+                                <a>
+                                  <FilterCenterFocusIcon className="product_icon" />
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        );
+                      })}
+                  </CarouselMulti>
+                </div>
+              )}
+                                                              <div className="end"></div>
+            </div>
+          );
+        })}
+      </Layout>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    type: state.type,
+  };
+};
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    onTypeAPI: () => {
+      dispatch(actions.typeAPI());
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- src/containers/ProductListPage/ProductPage/ProductPage.js
+++ src/containers/ProductListPage/ProductPage/ProductPage.js
@@ -1,74 +1,246 @@
 import React, { Component } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 import { connect } from "react-redux";
+import "./styles.css";
+import { Card, ThemeProvider } from "react-bootstrap";
 import * as actions from "./../../../actions/index";
-import { ImageTwoTone } from "@material-ui/icons";
+import Star from "@material-ui/icons/Star";
+import { Link, Redirect } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import StarBorder from "@material-ui/icons/StarBorder";
+import CheckCircleOutline from "@material-ui/icons/CheckCircleOutline";
 class ProductPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      sortPrice: [
+        {
+          price: 500,
+        },
+        {
+          price: 1000,
+        },
+      ],
+    };
   }
+  // onClick = (id, slug) => {
+  //   alert("aaa");
+  //   return <Redirect to={`/${slug}/${id}/p`}></Redirect>;
+  // }
+  onClick = (values) => {
+    var object = {};
+    var {match, category} = this.props;
+    var name = match.params.slug;
+    var idParent;
+    var type;
+    category.forEach((values, index) => {
+      if(values.name.toLowerCase() === name) {
+        idParent = values.idParent;
+      }
+    });
+    console.log(idParent);
+    category.forEach((values, index) => {
+      if(values.id === idParent) {
+        type = values.name;
+      }
+    });
+    console.log(values);
+    object = {
+      id: values.id,
+      name: values.name,
+      price: values.price,
+      img: values.productPictures[0].img,
+      quantity : 1,
+      nameIdParent : type
+    };
+    console.log(object);
+    this.props.onCart(object);
+  }
+  showStar = (data) => {
+    var result = [];
+    for (var i = 0; i < data; i++) {
+      result.push(<Star className="star" />);
+    }
+    for (var j = 0; j < 5 - data; j++) {
+      result.push(<StarBorder />);
+    }
+    return result;
+  };
   componentDidMount() {
-    this.props.onTypeAPI();
+    this.props.onProductAPI();
+    this.props.onCategoryAPI();
   }
+  showProduct = (product, category, match, slug, slugProduct, cart) => {
+    var result = product.map((values, index) => {
+      var temp = 0;
+      category.forEach((categoryValues, index) => {
+        if (values.categoryID === categoryValues.id) {
+          console.log(categoryValues.name.toLowerCase());
+          slug = categoryValues.name.toLowerCase();
+        }
+      });
+      if (match.params.slug === slug) {
+        return (
+          <Card
+            key={index}
+            border="primary"
+            className="card_parent"
+            style={{ width: "18rem" }}
+          >
+            <di className="home-product-item__favourite product">
+              <CheckCircleOutline
+                className="checkOutline"
+                style={{ fontSize: 17.5 }}
+              />
+              favourite
+            </di>
+
+            <di className="home-product-item__sale-off product">
+              <span className="home-product-item__sale-off-percent">
+                {values.sales}%
+              </span>
+              <span className="home-product-item__sale-off-label">SALE</span>
+            </di>
+            <Link
+              style={{ textDecoration: "none" }}
+              to={`/${match.params.slug}/${values.id}/p`}
+            >
+              <div className="productImgContainer product">
+                {values.productPictures.map((valuess, index3) => {
+                  var index = valuess.img.indexOf("samsung");
+                  if (index === 1) {
+                  }
+                  //var image = require(`./../../assets/images/${values.productPictures[index].img}`);
+                  var image = require(`./../../../assets/images/${valuess.img}`);
+                  return (
+                    <img
+                      key={index3}
+                      className="img"
+                      //src={`${process.env.PUBLIC_URL}/${truoc}`}
+                      src={image.default}
+                      alt="logo"
+                    />
+                  );
+                })}
+              </div>
+            </Link>
+            <Card.Body className="productInfo">
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/${match.params.slug}/${values.id}/p`}
+              >
+                <Card.Title className="productInfo_name product">
+                  {values.name}
+                </Card.Title>
+                <div className="productInfo_display product">
+                  <Card.Text>{this.showStar(values.star)}</Card.Text>
+                  <Card.Text className="showQuantity">
+                    ({values.quantity})
+                  </Card.Text>
+                </div>
+                <Card.Text className="productPrice product">
+                  <p>{values.price * ((100 - values.sales) / 100)}$</p>
+                  <p className="Oldprice">{values.price}$</p>
+                </Card.Text>
+              </Link>
+              {cart && cart.map((valuessss, index6) => {
+                if (valuessss.id === values.id) {
+                  temp = 1;
+                  return (
+                    <button key={index6} disabled className="add_to_cart in_cart">
+                      In Cart
+                    </button>
+                  );
+                }
+              })}
+              {temp === 0 ? (
+                <button className="add_to_cart" onClick={() => this.onClick(values)}>Add To Cart</button>
+              ) : (
+                ""
+              )}
+            </Card.Body>
+          </Card>
+        );
+      } else {
+        return null;
+      }
+    });
+    return result;
+  };
   render() {
-    var { type } = this.props;
-    console.log(type);
-    return (
-      <div>
-        {type.map((values, index) => {
-          return <h3 key={index}>{values.title}</h3>;
-        })}
-        {type.map((values, index) => {
-          return (
-            <Carousel key={index} renderThumbs={() => {}}>
-              {values.banners &&
-                values.banners.map((valuess, index) => {
-                  console.log(valuess.img);
-                  var image = require(`./../../../assets/images/${valuess.img}`);
-                  return (
-                    <a
-                      href={valuess.navigateTo}
-                      style={{ display: "block" }}
-                      key={index}
-                    >
-                      <img src={image.default} alt="" />
-                    </a>
-                  );
-                })}
-            </Carousel>
-          );
-        })}
-        {type.map((values, index) => {
-          return (
-            <div key={index}> 
-              {values.products &&
-                values.products.map((valuess, index1) => {
-                  var image = require(`./../../../assets/images/${valuess.img}`);
-                  return (
-                    <div key={index1}>
-                      <img src={image.default} alt="" />
-                    </div>
-                  );
-                })}
-            </div>
-          );
-        })}
-      </div>
-    );
+    var { product, match, category, cart } = this.props;
+    console.log(this.props);
+    console.log(match);
+    var { sortPrice } = this.state;
+    var slug = "";
+    var temp = 0, temp2 = 0;
+    var array1 = [];
+    var array2 = [];
+    var result = product.filter((values, index) => {
+      if (values.price >= 0 && values.price <= 500) {
+        return array1.push(values);
+      } else if (values.price > 500 && values.price <= 1000) {
+        return array2.push(values);
+      }
+    });
+    console.log(array1);
+    console.log(array2);
+    var showProduct = sortPrice.map((valuessss, index3) => {
+      console.log(match.params.slug);
+      console.log(slug);
+      return (
+        <Card key={index3}>
+          <Card.Header as="h5" className="cardHeader">
+            <Card.Text className="cartHeader_marginTop">
+              {match.params.slug} - under {valuessss.price}$
+            </Card.Text>
+            <button className="add_to_cart">View All</button>
+          </Card.Header>
+
+          <Card.Body className="show_product">
+            {valuessss.price >= 0 && valuessss.price <= 500
+              ? this.showProduct(
+                  array1,
+                  category,
+                  match,
+                  slug,
+                  match.params.slug,
+                  cart
+                )
+              : this.showProduct(
+                  array2,
+                  category,
+                  match,
+                  slug,
+                  match.params.slug,
+                  cart
+                )}
+          </Card.Body>
+        </Card>
+      );
+    });
+    console.log(showProduct);
+    return <div>{showProduct}</div>;
   }
 }
+
 const mapStateToProps = (state) => {
   return {
-    type: state.type,
+    product: state.product,
+    category: state.category,
+    cart : state.cart
   };
 };
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    onTypeAPI: () => {
-      dispatch(actions.typeAPI());
+    onProductAPI: () => {
+      dispatch(actions.productAPI());
     },
+    onCategoryAPI: () => {
+      dispatch(actions.categoryAPI());
+    },
+    onCart : (data) => {
+      dispatch(actions.cartReducers(data));
+    }
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(ProductPage);
