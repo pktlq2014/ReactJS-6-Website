60388936ff1c46d643d89a23cd0603b63ce6ec94
unknown <nguyenvanty10987654321000@gmail.com>
2021-03-22 14:06:52 +0700

    update 129

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -13,7 +13,7 @@ import ItemsCarousel from "react-items-carousel";
 import CreateIcon from "@material-ui/icons/Create";
 import * as actions from "./../../actions/index";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import { ImageTwoTone } from "@material-ui/icons";
+import { ArrowRightAltTwoTone, ImageTwoTone } from "@material-ui/icons";
 import Layout from "./../../components/Layout/layout";
 import FilterCenterFocusIcon from "@material-ui/icons/FilterCenterFocus";
 import SearchIcon from "@material-ui/icons/Search";
@@ -37,103 +37,127 @@ const responsive = {
 class HomePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      arrayProduct: [],
+      temp: 0,
+      data: [],
+    };
   }
   componentDidMount() {
+    this.props.onProductAPI();
     this.props.onTypeAPI();
   }
   render() {
-    var { type } = this.props;
+    var { type, product, category } = this.props;
+    console.log(category);
+    console.log(product);
     console.log(type);
+    var data = [];
+    var temp = 0;
+    if (temp === 0) {
+      category.forEach((values, index) => {
+        var temp = 0;
+        product.forEach((valuess, index) => {
+          if (values.id === valuess.parentID) {
+            if (temp === 0) {
+              data.push({ id: values.id, name: values.name });
+              temp = 1;
+            }
+          }
+        });
+      });
+      temp = 1;
+    }
+    console.log(data);
     return (
       <Layout>
         {type.map((values, index) => {
           return (
+            <Carousel
+              key={index}
+              className="home_carousel"
+              renderThumbs={() => {}}
+              autoPlay={true}
+              emulateTouch={true}
+              infiniteLoop={true}
+              interval={1750}
+            >
+              {values.banners &&
+                values.banners.map((valuess, index99) => {
+                  console.log(valuess.img);
+                  var image = require(`./../../assets/images/${valuess.img}`);
+                  return (
+                    <a
+                      href={valuess.navigateTo}
+                      style={{ display: "block" }}
+                      key={index99}
+                    >
+                      <img src={image.default} alt="" />
+                    </a>
+                  );
+                })}
+            </Carousel>
+          );
+        })}
+        {data.map((values, index) => {
+          return (
             <div key={index}>
-              <Carousel
-                key={index}
-                className="home_carousel"
-                renderThumbs={() => {}}
-                autoPlay={true}
-                emulateTouch={true}
-                infiniteLoop={true}
-                interval={1750}
-              >
-                {values.banners &&
-                  values.banners.map((valuess, index) => {
-                    console.log(valuess.img);
-                    var image = require(`./../../assets/images/${valuess.img}`);
-                    return (
-                      <a
-                        href={valuess.navigateTo}
-                        style={{ display: "block" }}
-                        key={index}
-                      >
-                        <img src={image.default} alt="" />
-                      </a>
-                    );
-                  })}
-              </Carousel>
-
-              {values.products.length <= 0 ? (
-                ""
-              ) : (
-                <div className="home_product">
-                  <div className="home_product_title">
-                    <div className="home_product_title_left">
-                      {values.title !== "" ? values.title : ""}
-                    </div>
-                    <button className="home_product_title_right">
-                      View All
-                    </button>
+              <div className="home_product">
+                <div className="home_product_title">
+                  <div className="home_product_title_left">
+                    {values.name !== "" ? values.name : ""}
                   </div>
-                  <div className="home_product_divide"></div>
-                  <CarouselMulti
-                    responsive={responsive}
-                    swipeable={false}
-                    draggable={false}
-                    // showDots={true}
-                    ssr={true} // means to render carousel on server-side.
-                    // infinite={true}
-                    transitionDuration={500}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
-                    containerClass="carousel-container"
-                    keyBoardControl={true}
-                  >
-                    {values.products &&
-                      values.products.map((valuess, index1) => {
-                        var image = require(`./../../assets/images/${valuess.img}`);
-                        return (
-                          <div className="product" key={index1}>
-                            <div className="product__header">
-                              <img src={image.default} alt="" />
-                            </div>
-
-                            <ul>
-                              <li>
-                                <a>
-                                  <AddShoppingCartIcon className="product_icon" />
-                                </a>
-                              </li>
-                              <li>
-                                <a>
-                                  <SearchIcon className="product_icon" />
-                                </a>
-                              </li>
-                              <li>
-                                <a>
-                                  <FilterCenterFocusIcon className="product_icon" />
-                                </a>
-                              </li>
-                            </ul>
-                          </div>
-                        );
-                      })}
-                  </CarouselMulti>
+                  <button className="home_product_title_right">View All</button>
                 </div>
-              )}
-                                                              <div className="end"></div>
+                <div className="home_product_divide"></div>
+                <CarouselMulti
+                  responsive={responsive}
+                  swipeable={false}
+                  draggable={false}
+                  // showDots={true}
+                  ssr={true} // means to render carousel on server-side.
+                  // infinite={true}
+                  transitionDuration={500}
+                  dotListClass="custom-dot-list-style"
+                  itemClass="carousel-item-padding-40-px"
+                  containerClass="carousel-container"
+                  keyBoardControl={true}
+                >
+                  {product.map((valuess, index2) => {
+                    if (values.id === valuess.parentID) {
+                      var image = require(`./../../assets/images/${valuess.productPictures[0].img}`);
+                      return (
+                        <div className="product" key={index2}>
+                          <div className="product__header">
+                            {image && (
+                              <img src={image.default} alt={image.default} />
+                            )}
+                          </div>
+
+                          <ul>
+                            <li>
+                              <a>
+                                <AddShoppingCartIcon className="product_icon" />
+                              </a>
+                            </li>
+                            <li>
+                              <a>
+                                <SearchIcon className="product_icon" />
+                              </a>
+                            </li>
+                            <li>
+                              <a>
+                                <FilterCenterFocusIcon className="product_icon" />
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                      );
+                    }
+                  })}
+                </CarouselMulti>
+              </div>
+              <div className="end"></div>
             </div>
           );
         })}
@@ -144,12 +168,17 @@ class HomePage extends Component {
 const mapStateToProps = (state) => {
   return {
     type: state.type,
+    product: state.product,
+    category: state.category,
   };
 };
 const mapDispatchToProps = (dispatch, props) => {
   return {
     onTypeAPI: () => {
       dispatch(actions.typeAPI());
+    },
+    onProductAPI: () => {
+      dispatch(actions.productAPI());
     },
   };
 };
